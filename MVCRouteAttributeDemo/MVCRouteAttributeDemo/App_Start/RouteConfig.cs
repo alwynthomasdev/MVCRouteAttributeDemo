f30cfadbf5cf8f984b1f8cb66304666da9a96dce
Alwyn Thomas <alwynthomas@live.co.uk>
2016-01-27 21:28:40 +0000

    a few little changes

--- MVCRouteAttributeDemo/MVCRouteAttributeDemo/App_Start/RouteConfig.cs
+++ MVCRouteAttributeDemo/MVCRouteAttributeDemo/App_Start/RouteConfig.cs
@@ -11,19 +11,14 @@ namespace MVCRouteAttributeDemo
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
             //IMPORTANT: Enable attribute routing.
             routes.MapMvcAttributeRoutes();
 
-            //routes.MapRoute(
-            //    name: "Default",
-            //    url: "{controller}/{action}/{id}",
-            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            //);
-
-            
-
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
         }
     }
 }
